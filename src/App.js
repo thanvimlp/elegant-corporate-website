9633d78b55490dbe9eb52b49b5431b56120a95fe
thanvimlp <thanvimlp@gmail.com>
2025-07-07 21:50:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HeroSection from "./HeroSection";
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
