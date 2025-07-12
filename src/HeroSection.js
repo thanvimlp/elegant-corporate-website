c59176d5128d8b7706c4c708a5ac77aa44fbff64
thanvimlp <thanvimlp@gmail.com>
2025-07-12 19:58:51 +0530

    Hero section layout completed without media query

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -105,7 +105,20 @@ function HeroSection() {
           </div>
         </div>
         <div className="hero-right">
+          <div className="background-border"></div>
           <img className="hero-img" src="/images/hero-img.jpeg"></img>
+          <button className="overlay-button">
+            <svg
+              class="c0jld cheux cbao0 cq71k cuz7k cevzp"
+              viewBox="0 0 88 88"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                class="play"
+                d="M52 44a.999.999 0 00-.427-.82l-10-7A1 1 0 0040 37V51a.999.999 0 001.573.82l10-7A.995.995 0 0052 44V44c0 .001 0 .001 0 0z"
+              ></path>
+            </svg>
+          </button>
         </div>
       </div>
     </div>
